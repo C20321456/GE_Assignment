71e5803adc43cf56101afb4270272b333d0ad28f
Michael Cooper <C20321456@mytudublin.ie>
2023-12-18 23:12:38 +0000

    table was given bounce function to make the ball bounce more

--- bilateral integration/AI_Paddle.gd
+++ bilateral integration/AI_Paddle.gd
@@ -1,4 +1,4 @@
-extends RigidBody3D
+extends CharacterBody3D
 
 # Adjust this speed value according to your AI preference
 var speed = 200
@@ -9,12 +9,6 @@ func _ready():
 	for node in get_tree().get_nodes_in_group("balls"):
 		if node.filename.ends_with("ping_pong_ball.tscn"):
 			balls.append(node)
-	
-	PhysicsServer3D.body_set_axis_lock(get_rid(), 2, true)
-	# Locks movement along the Z-axis. so it always stays in front of the table
-	#0 for X-axis
-	#1 for Y-axis
-	#2 for Z-axis
 
 func _process(delta):
 	var closest_ball = null
@@ -30,8 +24,14 @@ func _process(delta):
 	
 	if closest_ball:
 		var closest_ball_position = closest_ball.global_transform.origin
-   
-		if closest_ball_position.z > global_transform.origin.z:
-			# Move towards the ball only along the X-axis
-			var direction = Vector3(closest_ball_position.x - global_transform.origin.x, 0, 0).normalized()
-			move_and_collide(direction * speed * delta)
+		
+		# Calculate the direction to move towards the ball disregarding the Y-axis
+		var direction = (closest_ball_position - global_transform.origin)
+		direction.y = 0  # Disregard the Y-axis
+		direction = direction.normalized()
+		
+		# Make the AI paddle face the ball
+		look_at(closest_ball_position, Vector3.UP)
+		
+		# Move the AI paddle towards the ball 
+		move_and_slide()
---
+++ bilateral integration/scenes/audio_demo/objects/bounce.gd
@@ -0,0 +1,15 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var static_body = get_node("StaticBody3D")  # Assuming StaticBody3D is the parent of CollisionShape3D
+
+	if static_body:
+		var physics_material = PhysicsMaterial.new()
+		physics_material.bounce = 10.0  # Adjust this value to your desired bounciness
+
+		# Apply the physics material to the StaticBody3D
+		static_body.set_physics_material_override(physics_material)
+	else:
+		print("StaticBody3D node not found!")
